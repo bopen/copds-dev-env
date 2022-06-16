d915947cf4341591dfcbe04f2f1a6c34c74a7e63
Alessandro Amici <a.amici@bopen.eu>
2022-06-16 14:46:29 +0200

    Start working on sample data

--- pcstac/data-files/loadtestdata.py
+++ pcstac/data-files/loadtestdata.py
@@ -5,10 +5,10 @@ import orjson
 from pypgstac.load import Loader, Methods, PgstacDB
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data-files")
-collection = os.path.join(DATA_DIR, "reanalysis-era5-single-levels/collection.json")
+COLLECTION = os.path.join(DATA_DIR, "reanalysis-era5-single-levels/collection.json")
 
 
-def load_test_data() -> None:
+def load_test_data(collection) -> None:
     with PgstacDB() as conn:
         loader = Loader(db=conn)
         with open(collection, "rb") as f:
@@ -16,4 +16,4 @@ def load_test_data() -> None:
             loader.load_collections([c], Methods.upsert)
 
 
-load_test_data()
+load_test_data(COLLECTION)
